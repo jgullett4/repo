50be3c49a21fdcfcffae1d735dffed21389a4a23
BA201 <BA201@MCBA201-C28.classrooms.stlcc>
2017-02-15 13:26:59 -0600

    add another line in main class

--- src/consoleUI/Console.java
+++ src/consoleUI/Console.java
@@ -5,5 +5,6 @@ package consoleUI;
 public class Console {
     public static void main(String[] args) {
         System.out.println("Hello Github!");
+        System.out.println("Yay, I created a repo on Github!");
     }
 }
